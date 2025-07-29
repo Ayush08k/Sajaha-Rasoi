54f34f26c5fd2fbea8017a095e792345d002b0ac
Mohitjangra19 <mjsg46000@gmail.com>
2025-07-29 13:57:11 +0530

    Revert "component is added"
    This reverts commit 5f8b687fab231985513d0c6bc1084683a8ac18c5.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter, withHashLocation, withInMemoryScrolling } from '@angular/router';
+import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
@@ -8,10 +8,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(
-      routes,
-      withInMemoryScrolling({ scrollPositionRestoration: 'top' })
-    ),
-    provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay())
   ]
 };
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-// https://www.builder.io/c/docs/using-your-api-key
-export const environment = {
-  production: false,
-  builderApiKey: "2fdd2dc6f619474cbaec9f94a8f22abe",
-};
